142166ce35d2562f876a8717b74dc2a45a5c050f
DavidG <egonzalez80628@ufide.ac.cr>
2025-02-12 20:55:09 -0600

    Sem04

---
+++ Sem04/Tienda-David Gonzalez/src/main/java/Portafolio_SebastianMendez/Portafolio_SebastianMendez/PortafolioSebastianMendezApplication.java
@@ -0,0 +1,13 @@
+package Portafolio_SebastianMendez.Portafolio_SebastianMendez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioSebastianMendezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioSebastianMendezApplication.class, args);
+	}
+
+}
---
+++ Sem04/Tienda-David Gonzalez/src/test/java/Portafolio_DavidGonzalez/Portafolio_DavidGonzalez/PortafolioDavidGonzalezApplicationTests.java
@@ -0,0 +1,13 @@
+package Portafolio_DavidGonzalez.Portafolio_DavidGonzalez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioDavidGonzalezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
